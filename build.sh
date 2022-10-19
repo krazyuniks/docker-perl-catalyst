eb6dd1c56d236e2b523ab17b862012ee5b482a68
Ryan Lauterbach <ryan@radianit.com>
2022-10-19 18:37:45 +0100

    test

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# for manual building on local machine.
+# use git tags to initiate github actions build, see .github/workflows/
+
 set -e
 
 set -o allexport; source environment.sh; set +o allexport
+
+CPAN_MODULES=$(grep -E '^[A-Z].+' cpan_modules.txt | tr '\n' ' ')
 
 docker build -t ryanlauterbach/perl-catalyst:latest -t ryanlauterbach/perl-catalyst:${PERL_DOCKER_TAG} --build-arg PERL_DOCKER_TAG=${PERL_DOCKER_TAG} --build-arg CPAN_MODULES="${CPAN_MODULES}" .
 
--- environment.sh
+++ environment.sh
@@ -1,2 +1,1 @@
 PERL_DOCKER_TAG=5.36.0-slim-bullseye
-CPAN_MODULES=$(grep -E '^[A-Z].+' cpan_modules.txt | tr '\n' ' ')
