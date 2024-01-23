a0493bf09da4c89b23b74e7ba7d9a84605b735bd
Ryan Lauterbach <ryan@radianit.com>
2024-01-23 12:30:42 +0000

    default number of cpm workers

--- install_cpan_modules.sh
+++ install_cpan_modules.sh
@@ -5,5 +5,5 @@ set -e
 grep -E '^[A-Z].+' ${1:=cpan_modules.txt} | xargs -n10 | while read mod
 do
     echo "Installing perl modules: $mod"
-    cpm install -w 16 --show-build-log-on-failure --global $mod
+    cpm install --show-build-log-on-failure --global $mod
 done
