42d5351b0e98e6a9476c78f17b03038cc2b53da9
Ryan Lauterbach <ryan@radianit.com>
2024-12-06 14:17:01 +0000

    env updates

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ set -o allexport; source environment.sh; set +o allexport
 
 CPAN_MODULES=$(grep -E '^[A-Z].+' cpan_modules.txt | tr '\n' ' ' | sed -re 's/\s+$//g')
 
-docker build -t ryanlauterbach/perl-catalyst:latest -t ryanlauterbach/perl-catalyst:${PERL_VERSION} --build-arg PERL_VERSION=${PERL_VERSION} --build-arg CPAN_MODULES="${CPAN_MODULES}" .
+docker build -t ryanlauterbach/perl-catalyst:latest --build-arg CPAN_MODULES="${CPAN_MODULES}" .
 
 docker push --all-tags ryanlauterbach/perl-catalyst
